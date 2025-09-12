587490f33ec9346a07383f982b804c26367836aa
Samruddhi <patilsamruddhi42@gmail.com>
2025-09-12 16:58:19 +0530

    react lifecycle

--- src/About.js
+++ src/About.js
@@ -1,12 +1,25 @@
+import React from "react";
 import User from "./User" ;
 import UserClass from "./UserClass" 
-  const About = ()=>{
-    return(
-        <div className="mt-5">
+
+class About extends React.Component{
+    constructor(props){
+     super(props)
+     console.log("parent Constructor called...");
+    }
+
+    componentDidMount(){
+        console.log("parent component did mount call...");
+    }
+    render(){
+        console.log("parent Render called...");
+        return(
+             <div className="mt-5">
          <h2>About Class based component</h2>
-          <User name={"samruddhi"} location={"pune"} /><br/>
+          {/* <User name={"samruddhi"} location={"pune"} /><br/> */}
           <UserClass name={"samruddhi"} location={"pune"}/>
         </div>
-    )
-  }
+        )
+    }
+}
   export default About;
--- src/UserClass.js
+++ src/UserClass.js
@@ -4,19 +4,34 @@ class UserClass extends React.Component {
     //class based component which is exntends from React.Component and this React.Component is basically part of react library means basically commming from ract packages
     constructor(props){
       super(props)
-      console.log(props);
+      //console.log(props);
 
       this.state= {
         count:0
       }
+      console.log("child constructor called...");
+      
     }
 
+    componentDidMount(){
+      console.log("child component did mount"); 
+    }
   render() {
+      console.log("childe render called...")
+
     const {name,location,contact} = this.props
     const {count} = this.state
+
     return (
       <div className="border border-black w-96">
         <h2>Count: {count}</h2>
+        <button className="border border-black"
+         onClick={()=>{
+          this.setState({
+           count : this.state.count+1
+        })}}>
+          Count Increse
+          </button>
         <h2>Name:{name} </h2>
         <h3>Location:{location} </h3>
         <h4>Contact: patil27@gamil.com</h4>
